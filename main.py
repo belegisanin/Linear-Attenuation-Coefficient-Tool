d9df30a8857f661c0134a0f9293b877aee5fc9bc
belegisanin <belegisanin@outlook.com>
2022-05-27 11:40:33 +0200

    matplotlib added basic show_plot axes generator

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
 welcome_msg = "#" * 50 + "\nLinear Attenuation Coefficient Calculation, Visualisation and Analysis Tool.\n" + "#" * 50
 measurements = []
+
+
+def generate_axes(ms_data):
+    x_ax = y_ax = []
+    for dp in ms_data:
+        x, y = dp
+        x_ax.append(x)
+        y_ax.append(y)
+
+    return x_ax, y_ax
+
+
+def show_plot(measurement_data):
+    plt.style.use('_mpl-gallery')
+    x_axes, y_axes = generate_axes(measurement_data)
+
+    fig, ax = plt.subplots()
+
+    plt.xlabel("x [cm]")
+    plt.ylabel("N [imp/min]")
+
+    #ax.set_xlabel("x [cm]")
+    #ax.set_ylabel("N [imp/min]")
+
+    ax.scatter(x_axes, y_axes)
+    plt.show()
 
 
 def measurements_input():
@@ -26,3 +56,5 @@ if __name__ == '__main__':
     print("Enter 'END' to end input.", "\n")
 
     measurements_input()
+
+    show_plot(measurements)
